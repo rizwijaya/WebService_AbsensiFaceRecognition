5a7cbcd684ceaaa996cba43942089d514d7cfb29
Rizqi Wijaya <rizwijaya@github.com>
2021-08-22 19:12:50 +0700

    Create a features course that students take.

--- application/controllers/Siswa.php
+++ application/controllers/Siswa.php
@@ -3,8 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Siswa extends CI_Controller {
 
-	public function __construct()
-	{
+	public function __construct() {
 		parent::__construct();
 		$this->load->library('session');
 		if (!$this->session->userdata('no_induk')) {
@@ -17,11 +16,21 @@ class Siswa extends CI_Controller {
 		}
 	}
 
-	public function index()
-	{
+	public function index() {
 		$this->load->view('template/dashboard/header');
         $this->load->view('template/dashboard/sidebar');
         $this->load->view('siswa/index');
         $this->load->view('template/dashboard/footer');
+	}
+
+	public function matakuliah() {
+		$id_user = $this->session->userdata('id_user');
+        $this->load->model('siswa_model');
+        $data['matkul'] = $this->siswa_model->matakuliah($id_user);
+
+        $this->load->view('template/dashboard/header');
+        $this->load->view('template/dashboard/sidebar');
+        $this->load->view('siswa/matakuliah', $data);
+        $this->load->view('template/dashboard/footer');  
 	}
 }
---
+++ application/views/siswa/matakuliah.php
@@ -0,0 +1,49 @@
+        </div>
+        </div>
+        </div>
+        <!-- Page content -->
+        <div class="container-fluid mt--6">
+            <!-- Content -->
+            <div class="card mb-12">
+                <!-- Table dimulai -->
+                <div class="card-header border-0">
+                    <div class="row align-items-center">
+                        <div class="col">
+                            <h3 class="mb-0">Data Mata Kuliah</h3>
+                        </div>
+                        <!-- Button trigger modal -->
+                        <!-- <div class="col text-right">
+                            <a class="btn btn-primary mb-0" href="daftarpeserta/cetak_sesi/"><i class="fa fa-print"></i> Cetak Laporan</a>
+                        </div> -->
+                    </div>
+                </div>
+                <!-- Isi Tabel -->
+                <div class="table-responsive">
+                    <table class="table align-items-center table-flush">
+                        <thead class="thead-light">
+                            <tr>
+                                <th scope="col" class="sort" data-sort="no">No</th>
+                                <th scope="col" class="sort" data-sort="mata_kuliah">Mata Kuliah</th>
+                                <th scope="col" class="sort" data-sort="dosen"> Nama Dosen</th>
+                                <th scope="col" class="sort" data-sort="mulai_kuliah">Hari</th>
+                                <th scope="col" class="sort" data-sort="mulai_kuliah">Mulai Kuliah</th>
+                                <th scope="col" class="sort" data-sort="selesai_kuliah">Selesai Kuliah</th>
+                            </tr>
+                        </thead>
+                        <tbody class="list">
+                            <tr>
+                                <?php $no =1; foreach ($matkul as $mk) : ?>
+                                <th scope="row"><?= $no++;?></th>
+                                <td><?= $mk['nama_matkul']; ?></td>
+                                <td><?= $mk['dosen'];?></td>
+                                <td><?= $mk['hari_kuliah']; ?></td>
+                                <td><?= $mk['start_kuliah']; ?> WIB</td>
+                                <td><?= $mk['end_kuliah'];?> WIB</td>
+                            </tr>
+                            <?php endforeach; ?>
+                        </tbody>
+                    </table>
+                </div>
+                <!-- Ini table nya -->
+            </div> <!-- Div Class Container Content-->
+            <!-- End Content -->
